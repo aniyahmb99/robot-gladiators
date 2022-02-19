f8111238aee591c11c72e666669e9100683fb28a
aniyahmb99 <aniyah_mb@yahoo.com>
2022-02-19 10:57:55 -0500

    for LA

--- assets/js/game.js
+++ assets/js/game.js
@@ -203,8 +203,8 @@ switch (shopOptionPrompt) {
 }
 }
 // function to generate a random numeric value
-var randomNumber = function(40, 60) {
-    var value = Math.floor(Math.random() * (60 - 40 +1) + 40);
+var randomNumber = function(min, max) {
+    var value = Math.floor(Math.random() * (max - min +1) + min);
 
     return value;
 }
