7e27ac07cab5ba485410490d0a70fb5f801961da
aniyahmb99 <aniyah_mb@yahoo.com>
2022-02-20 12:53:19 -0500

    to fix vscode errors

--- assets/js/game.js
+++ assets/js/game.js
@@ -35,16 +35,23 @@ promptFight = promptFight.toLowerCase();
   }
 // fight function (now with parameter for enemy's name)
 var fight = function(enemy) {
+    // keep track of who goes first
+    var isPlayerTurn = true;
+
+    // randomly change turn order
+    if (Math.random() > 0.5) {
+        isPlayerTurn = false;
+    }
     // repeat and execute as long as the enemy-robot is alive
     while(playerInfo.health > 0 && enemy.health > 0) {
         // ask player if they'd like to fight or skip using fightOrSkip function
-       if (fightOrSkip()); {
+       if (fightOrSkip()) {
            // if true, leave fight by breaking loop
            break;
        }
         var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
-    // remove enemy's health by subtracting the amount set in the playerAttack variable
-    enemy.health = Math.max(0, enemy.health - playerInfo.attack);
+    // remove enemy's health by subtracting the amount we set in the damage variable
+    enemy.health = Math.max(0, enemy.health - damage);
     console.log(
         playerInfo.name + " attacked " + enemy.name + ". " + enemy.name + " now has "
  + enemy.health + " health remaining.");
@@ -62,9 +69,12 @@ var fight = function(enemy) {
     else {
         window.alert(enemy.name + " still has " + enemy.health + " health left.");
     }
+    // player gets attacked first
+ } else {
     // Subtract the value of 'enemy.attack' from the value of 'playerHealth' and use that result to update the value in the 'playerHealth' variable.
         var damage = randomNumber(enemy.attack - 3, enemy.attack);
 
+        // remove player's health by subtracting the amount we set in the damage variable
         playerInfo.health = Math.max(0, playerInfo.health - damage);
     // Log a resulting message to the console so we know that it worked.
     console.log(
@@ -74,11 +84,15 @@ var fight = function(enemy) {
     // check player's health
     if (playerInfo.health <= 0) {
         window.alert(playerInfo.name + " has died!");
+        // leave while() loop if player is dead
         break;
     } else {
         window.alert(playerInfo.name + " still has " + playerInfo.health + " health left.");
       }
     }
+    // switch turn order for next round
+    isPlayerTurn = !isPlayerTurn;
+}
 };
 
     // fight each enemy-robot by looping over them and fighting them one at a time
